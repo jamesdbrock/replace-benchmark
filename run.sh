59eb0ab22d0f74b7208558bf1420671a0e19f838
James Brock <jamesbrock@gmail.com>
2019-10-10 11:03:54 +0900

    run.sh output format

--- run.sh
+++ run.sh
@@ -12,15 +12,19 @@ mkdir -p bin
 
 cabal v2-install megaparsec-string megaparsec-bytestring megaparsec-text attoparsec-bytestring attoparsec-text --symlink-bindir=./bin
 
-# diff <(sed 's/foo/bar/g' < bench-test.txt) <(cabal v2-run bench-string < bench-test.txt)
-# diff <(sed 's/foo/bar/g' < bench-test.txt) <(cabal v2-run bench-bytestring < bench-test.txt)
+OPTIONS="--field-separator , --event=task-clock"
 
-OPTIONS="--metric-only"
-
+echo -n "sed                   "
 perf stat ${OPTIONS} sed 's/fnord/bar/g' < bench-test.txt 1> /dev/null
+echo -n "megaparsec-string     "
 perf stat ${OPTIONS} bin/megaparsec-string < bench-test.txt 1> /dev/null
+echo -n "megaparsec-bytestring "
 perf stat ${OPTIONS} bin/megaparsec-bytestring < bench-test.txt 1> /dev/null
+echo -n "attoparsec-bytestring "
 perf stat ${OPTIONS} bin/attoparsec-bytestring < bench-test.txt 1> /dev/null
+echo -n "megaparsec-text       "
 perf stat ${OPTIONS} bin/megaparsec-text < bench-test.txt 1> /dev/null
+echo -n "attoparsec-text       "
 perf stat ${OPTIONS} bin/attoparsec-text < bench-test.txt 1> /dev/null
+echo -n "python3               "
 perf stat ${OPTIONS} python3 replace-python.py < bench-test.txt 1> /dev/null
